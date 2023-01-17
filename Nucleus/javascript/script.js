5adfb8153e04a4f63dcf3dff2a0b8cf62b4eed4a
Sanket Dhuri <iamahacker.sanket@gmail.com>
2023-01-17 21:30:18 +0530

    Add files via upload

---
+++ Nucleus/javascript/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#color_mode").on("change", function () {
+        colorModePreview(this);
+    })
+});
+
+function colorModePreview(ele) {
+    if($(ele).prop("checked") == true){
+        $('body').addClass('dark-preview');
+        $('body').removeClass('white-preview');
+    }
+    else if($(ele).prop("checked") == false){
+        $('body').addClass('white-preview');
+        $('body').removeClass('dark-preview');
+    }
+}
