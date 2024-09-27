4c0e4fabefc99d10418c2afc2005705a58debd04
Sanket Dhuri <contact.sanketdhuri@gmail.com>
2024-09-27 16:20:03 +0530

    fix: smoth scrolling

--- Nucleus/javascript/script.js
+++ Nucleus/javascript/script.js
@@ -32,3 +32,17 @@ gsap.from(".box", {
 
 	}
 })
+
+    document.querySelectorAll('a.nav-hall').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                });
+            }
+        });
+    });
